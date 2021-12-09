9086de1d3aa7a93d038c7860faec67806133c874
angelinasantiago <77749659+angelinasantiago@users.noreply.github.com>
2021-12-08 21:45:07 -0500

    enemies disappear when bullet hits them

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -19,6 +19,10 @@ public class BulletScript : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.tag == "Enemy") 
+        {
+            Destroy(collision.gameObject);
+        }
         Destroy(gameObject);
     }
 }
