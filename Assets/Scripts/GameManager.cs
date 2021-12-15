05eae531807fe1deb006fc51a9a201f414ede636
angelinasantiago <77749659+angelinasantiago@users.noreply.github.com>
2021-12-15 16:12:27 -0500

    began adding to Game Manager script for the score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public int score;
+    public TextMeshPro scoretext;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        scoretext.text = score.ToString();
+    }
+
+    public void increasescore(int value)
+    {
+        score += value;
     }
 }
